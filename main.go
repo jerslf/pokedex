7436741a6083ac0ee01811360c64dec73ab64bc7
jerslf <jerslf@hotmail.co.uk>
2025-08-13 17:29:20 +0200

    Refactor main function to use startRepl for command input handling

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		ok := scanner.Scan()
-		if !ok {
-			break
-		}
-		input := scanner.Text()
-		text := cleanInput(input)
-		if len(text) == 0 {
-			fmt.Println("Input was empty")
-			continue
-		}
-		fmt.Printf("Your command was: %v\n", text[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +11,21 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
 }
