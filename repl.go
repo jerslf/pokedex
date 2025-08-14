5502afcbb5e95b10a89bf7d92550d7b628be3cfb
jerslf <jerslf@hotmail.co.uk>
2025-08-14 23:13:46 +0200

    Implement command handling for 'exit' and 'help' in REPL

--- repl.go
+++ repl.go
@@ -7,6 +7,47 @@ import (
 	"strings"
 )
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for key, value := range commands {
+		fmt.Printf("%v: %v\n", key, value.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
@@ -26,6 +67,15 @@ func startRepl() {
 
 		commandName := words[0]
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		c, exists := commands[commandName]
+		if exists {
+			err := c.callback()
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+
 	}
 }
